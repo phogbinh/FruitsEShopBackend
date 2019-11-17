fee24fc542e86732e51f310c289415ec3b332f59
kyo <phongbinh2511@gmail.com>
2019-11-17 16:22:53 +0800

    Made the attributes UserName and Password of the struct User to be 'binding: "required"' so that the function getUserFromContext() in database_users_table_handlers.go can operate properly.

--- model/user.go
+++ model/user.go
@@ -2,8 +2,8 @@ package model
 
 type User struct {
 	Mail         string `json:"mail"`
-	Password     string `json:"password"`
-	UserName     string `json:"userName"`
+	Password     string `json:"password" binding:"required"`
+	UserName     string `json:"userName" binding:"required"`
 	Nickname     string `json:"nickname"`
 	Fname        string `json:"fname"`
 	Lname        string `json:"lname"`
