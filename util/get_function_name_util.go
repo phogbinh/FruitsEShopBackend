8e356f5f45327509a9270743362a9e4876e3dbfb
kyo <phongbinh2511@gmail.com>
2019-11-29 20:59:57 +0800

    Main task: Create a method named 'GetErrorMessageHeaderContainingFunctionName' in the package 'util' that returns the header of an error message which contains a function name. Sub-task: Created the method 'GetErrorMessageHeaderContainingFunctionName'.

--- util/get_function_name_util.go
+++ util/get_function_name_util.go
@@ -5,6 +5,11 @@ import (
 	"runtime"
 )
 
+// GetErrorMessageHeaderContainingFunctionName returns the header of an error message which contains the given function name.
+func GetErrorMessageHeaderContainingFunctionName(functionInterface interface{}) string {
+	return "Error occurred at function [" + getFunctionName(functionInterface) + "]" + ColonSpace
+}
+
 func getFunctionName(functionInterface interface{}) string {
 	return runtime.FuncForPC(reflect.ValueOf(functionInterface).Pointer()).Name()
 }
