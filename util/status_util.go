41dec8d2bdb4f9aaa0af1fc7770e34c06e0ce62c
kyo <phongbinh2511@gmail.com>
2019-11-29 22:23:32 +0800

    Added StatusInternalServerError().

--- util/status_util.go
+++ util/status_util.go
@@ -13,6 +13,13 @@ func StatusOK() Status {
 		ErrorMessage:   ""}
 }
 
+// StatusInternalServerError returns an http.StatusInternalServerError associated with the error message consisting of the function name and the given error.
+func StatusInternalServerError(functionInterface interface{}, err error) Status {
+	return Status{
+		HttpStatusCode: http.StatusInternalServerError,
+		ErrorMessage:   GetErrorMessageHeaderContainingFunctionName(functionInterface) + err.Error()}
+}
+
 // IsStatusOK returns true if the given status is StatusOK.
 func IsStatusOK(status Status) bool {
 	return status.HttpStatusCode == http.StatusOK
