d9b4a3e7a397d5903085cb49d8b66a87bdeff99b
kyo <phongbinh2511@gmail.com>
2019-11-29 22:46:30 +0800

    Reallocated the functions getErrorMessageHeaderContainingFunctionName and getFunctionName to status_util.go. As a result, the now redundant get_function_name_util.go was deleted.

--- util/status_util.go
+++ util/status_util.go
@@ -2,6 +2,8 @@ package util
 
 import (
 	"net/http"
+	"reflect"
+	"runtime"
 
 	. "backend/model"
 )
@@ -32,4 +34,12 @@ func getErrorStatus(httpStatusCode int, functionInterface interface{}, err error
 	return Status{
 		HttpStatusCode: httpStatusCode,
 		ErrorMessage:   getErrorMessageHeaderContainingFunctionName(functionInterface) + err.Error()}
+}
+
+func getErrorMessageHeaderContainingFunctionName(functionInterface interface{}) string {
+	return "Error occurred at function [" + getFunctionName(functionInterface) + "]" + ColonSpace
+}
+
+func getFunctionName(functionInterface interface{}) string {
+	return runtime.FuncForPC(reflect.ValueOf(functionInterface).Pointer()).Name()
 }
