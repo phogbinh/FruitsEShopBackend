a6189c23d934a46a5b58bd2ca44e781cce3d24dd
kyo <phongbinh2511@gmail.com>
2019-11-29 20:57:53 +0800

    Main task: Create a method named 'GetErrorMessageHeaderContainingFunctionName' in the package 'util' that returns the header of an error message which contains a function name. Sub-task: Made 'GetFunctionName' a private method, I planned the sole caller of this function is 'GetErrorMessageHeaderContainingFunctionName'.

--- util/get_function_name_util.go
+++ util/get_function_name_util.go
@@ -5,7 +5,6 @@ import (
 	"runtime"
 )
 
-// GetFunctionName returns the name of the given function interface at runtime.
-func GetFunctionName(i interface{}) string {
+func getFunctionName(i interface{}) string {
 	return runtime.FuncForPC(reflect.ValueOf(i).Pointer()).Name()
 }
