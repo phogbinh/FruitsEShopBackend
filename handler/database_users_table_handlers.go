9d17a9ad8b628f2d8dda5154422da84dd560b2e0
kyo <phongbinh2511@gmail.com>
2019-11-30 10:43:54 +0800

    Updated getAllUsers() so that every table attributes correspond to a field of User struct.

--- handler/database_users_table_handlers.go
+++ handler/database_users_table_handlers.go
@@ -42,7 +42,7 @@ func getAllUsers(databaseUsersTableRowsPtr *sql.Rows) ([]User, Status) {
 	var users []User
 	for databaseUsersTableRowsPtr.Next() {
 		var user User
-		scanError := databaseUsersTableRowsPtr.Scan(&user.UserName, &user.Password)
+		scanError := databaseUsersTableRowsPtr.Scan(&user.Mail, &user.Password, &user.UserName, &user.Nickname, &user.Fname, &user.Lname, &user.Phone, &user.Location, &user.Money, &user.Introduction)
 		if scanError != nil {
 			return nil, util.StatusInternalServerError(getAllUsers, scanError)
 		}
