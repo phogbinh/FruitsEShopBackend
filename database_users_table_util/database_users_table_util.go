72fa7004c9f98ba5fc63027536a8495d6e49ce39
kyo <phongbinh2511@gmail.com>
2019-12-01 18:23:24 +0800

    Rewrote the create table SQL statements.

--- database_users_table_util/database_users_table_util.go
+++ database_users_table_util/database_users_table_util.go
@@ -19,16 +19,19 @@ const (
 func CreateDatabaseUsersTableIfNotExists(databasePtr *sql.DB) error {
 	_, createTableError := databasePtr.Exec("CREATE TABLE IF NOT EXISTS user_data.users" +
 		"(" +
-		MailColumnName + "			VARCHAR(320)	NOT NULL	," +
-		PasswordColumnName + "		VARCHAR(30)		NOT NULL	," +
-		UserNameColumnName + "		VARCHAR(30)		PRIMARY KEY	," +
-		NicknameColumnName + "		VARCHAR(30)					," +
-		FnameColumnName + "			VARCHAR(15)		NOT NULL	," +
-		LnameColumnName + "			VARCHAR(15)		NOT NULL	," +
-		PhoneColumnName + "			VARCHAR(30)					," +
-		LocationColumnName + "		VARCHAR(30)					," +
-		MoneyColumnName + "			DECIMAL(10, 2)				," +
-		IntroductionColumnName + "	VARCHAR(255)" +
+		MailColumnName + "			VARCHAR(320)	NOT NULL," +
+		PasswordColumnName + "		VARCHAR(30)		NOT NULL," +
+		UserNameColumnName + "		VARCHAR(30)		NOT NULL," +
+		NicknameColumnName + "		VARCHAR(30)				," +
+		FnameColumnName + "			VARCHAR(15)		NOT NULL," +
+		LnameColumnName + "			VARCHAR(15)		NOT NULL," +
+		PhoneColumnName + "			VARCHAR(30)				," +
+		LocationColumnName + "		VARCHAR(30)				," +
+		MoneyColumnName + "			DECIMAL(30, 2)			," +
+		IntroductionColumnName + "	VARCHAR(255)			," +
+		"PRIMARY KEY(" + UserNameColumnName + ")," +
+		"UNIQUE(" + MailColumnName + ")," +
+		"UNIQUE(" + NicknameColumnName + ")" +
 		");")
 	return createTableError
 }
