6cf92f2145f080d075e3d58fb8f5d0796c1800dd
kyo <phongbinh2511@gmail.com>
2019-12-07 20:31:24 +0800

    Added password comparison with that of user fetched from the database `users` table in login handler. Finished login handler.

--- handler/login_handler.go
+++ handler/login_handler.go
@@ -2,12 +2,12 @@ package handler
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
 
 	DUTU "backend/database_users_table_util"
 	"backend/middleware"
+	"backend/util"
 
 	"github.com/gin-gonic/gin"
 )
@@ -23,14 +23,18 @@ func LoginHandler(databasePtr *sql.DB) gin.HandlerFunc {
 			context.Status(http.StatusInternalServerError)
 			return
 		}
-
 		mail := context.PostForm(DUTU.MailColumnName)
 		password := context.PostForm(DUTU.PasswordColumnName)
-
-		fmt.Printf("mail is" + mail + "password is" + password)
-
+		user, status := DUTU.GetUserByMail(mail, databasePtr)
+		if !util.IsStatusOK(status) {
+			context.JSON(status.HttpStatusCode, gin.H{util.JsonError: status.ErrorMessage})
+			return
+		}
+		if user.Password != password {
+			context.Status(http.StatusBadRequest)
+			return
+		}
 		context.Status(http.StatusOK)
-
 		authMiddleware.LoginHandler(context)
 	}
 }
