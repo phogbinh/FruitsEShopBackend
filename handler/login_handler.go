e8daf84f5fada170ec9244a6ea99f3222058108b
kyo <phongbinh2511@gmail.com>
2019-12-07 20:52:35 +0800

    Utilized getLoginFromRequest in LoginHandler.

--- handler/login_handler.go
+++ handler/login_handler.go
@@ -24,14 +24,17 @@ func LoginHandler(databasePtr *sql.DB) gin.HandlerFunc {
 			context.Status(http.StatusInternalServerError)
 			return
 		}
-		mail := context.PostForm(DUTU.MailColumnName)
-		password := context.PostForm(DUTU.PasswordColumnName)
-		user, status := DUTU.GetUserByMail(mail, databasePtr)
+		login, getLoginStatus := getLoginFromRequest(context)
+		if !util.IsStatusOK(getLoginStatus) {
+			context.JSON(getLoginStatus.HttpStatusCode, gin.H{util.JsonError: getLoginStatus.ErrorMessage})
+			return
+		}
+		user, status := DUTU.GetUserByMail(login.Mail, databasePtr)
 		if !util.IsStatusOK(status) {
 			context.JSON(status.HttpStatusCode, gin.H{util.JsonError: status.ErrorMessage})
 			return
 		}
-		if user.Password != password {
+		if user.Password != login.Password {
 			context.Status(http.StatusBadRequest)
 			return
 		}
