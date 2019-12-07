ba1e9b7cc0e890a5e4a484b1876e56b59a39b6e1
kyo <phongbinh2511@gmail.com>
2019-12-07 19:15:46 +0800

    Renamed a constant to better clarify its usage.

--- handler/database_users_table_handlers.go
+++ handler/database_users_table_handlers.go
@@ -17,7 +17,7 @@ import (
 const (
 	queryInsertUser         = "INSERT INTO " + DUTU.TableName + " VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)"
 	queryGetAllUsers        = "SELECT * FROM " + DUTU.TableName
-	queryGetUser            = "SELECT * FROM " + DUTU.TableName + " WHERE " + DUTU.UserNameColumnName + " = ?"
+	queryGetUserByUserName  = "SELECT * FROM " + DUTU.TableName + " WHERE " + DUTU.UserNameColumnName + " = ?"
 	queryGetUserByMail      = "SELECT * FROM " + DUTU.TableName + " WHERE " + DUTU.MailColumnName + " = ?"
 	queryUpdateUserPassword = "UPDATE " + DUTU.TableName + " SET " + DUTU.PasswordColumnName + " = ? WHERE " + DUTU.UserNameColumnName + " = ?"
 	queryDeleteUser         = "DELETE FROM " + DUTU.TableName + " WHERE " + DUTU.UserNameColumnName + " = ?"
@@ -40,7 +40,7 @@ func CreateUserToDatabaseUsersTableAndRespondJsonOfUserHandler(databasePtr *sql.
 			context.JSON(insertStatus.HttpStatusCode, gin.H{util.JsonError: insertStatus.ErrorMessage})
 			return
 		}
-		getUser, getStatus := getUserByKeyColumnFromDatabaseUsersTable(queryGetUser, user.UserName, databasePtr)
+		getUser, getStatus := getUserByKeyColumnFromDatabaseUsersTable(queryGetUserByUserName, user.UserName, databasePtr)
 		if !util.IsStatusOK(getStatus) {
 			context.JSON(getStatus.HttpStatusCode, gin.H{util.JsonError: getStatus.ErrorMessage})
 			return
@@ -100,7 +100,7 @@ func getAllUsers(databaseUsersTableRowsPtr *sql.Rows) ([]User, Status) {
 func RespondJsonOfUserFromDatabaseUsersTableHandler(databasePtr *sql.DB) gin.HandlerFunc {
 	return func(context *gin.Context) {
 		userName := context.Param(DUTU.UserNameColumnName)
-		user, status := getUserByKeyColumnFromDatabaseUsersTable(queryGetUser, userName, databasePtr)
+		user, status := getUserByKeyColumnFromDatabaseUsersTable(queryGetUserByUserName, userName, databasePtr)
 		if !util.IsStatusOK(status) {
 			context.JSON(status.HttpStatusCode, gin.H{util.JsonError: status.ErrorMessage})
 			return
@@ -161,7 +161,7 @@ func UpdateUserPasswordInDatabaseUsersTableAndRespondJsonOfUserHandler(databaseP
 			context.JSON(updateStatus.HttpStatusCode, gin.H{util.JsonError: updateStatus.ErrorMessage})
 			return
 		}
-		getUser, getStatus := getUserByKeyColumnFromDatabaseUsersTable(queryGetUser, userName, databasePtr)
+		getUser, getStatus := getUserByKeyColumnFromDatabaseUsersTable(queryGetUserByUserName, userName, databasePtr)
 		if !util.IsStatusOK(getStatus) {
 			context.JSON(getStatus.HttpStatusCode, gin.H{util.JsonError: getStatus.ErrorMessage})
 			return
@@ -210,7 +210,7 @@ func deleteUserFromDatabaseUsersTable(userName string, databasePtr *sql.DB) Stat
 }
 
 func isExistingUserInDatabaseUsersTable(userName string, databasePtr *sql.DB) (bool, Status) {
-	users, getStatus := getUsersByKeyColumnFromDatabaseUsersTable(queryGetUser, userName, databasePtr)
+	users, getStatus := getUsersByKeyColumnFromDatabaseUsersTable(queryGetUserByUserName, userName, databasePtr)
 	if !util.IsStatusOK(getStatus) {
 		return false, getStatus
 	}
